619b6914cb0d0e38e5226f704a463831145478df
C20BR <tracelewallen5@gmail.com>
2026-09-24 21:04:25 -0400

    Update game filtering to use anchor tags

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const input = document.querySelector('input');
-const games = document.querySelectorAll('#games img');
+const games = document.querySelectorAll('#games a');
 
 input.addEventListener('input', () => {
-  const searchTerm = input.value.toLowerCase();
-  games.forEach(game => {
-    if (game.alt.toLowerCase().includes(searchTerm)) {
-      game.style.display = 'block';
-    } else {
-      game.style.display = 'none';
-    }
-  });
+    const searchTerm = input.value.toLowerCase();
+
+    games.forEach(game => {
+        const name = game.querySelector('img').alt.toLowerCase();
+
+        if (name.includes(searchTerm)) {
+            game.style.display = '';
+        } else {
+            game.style.display = 'none';
+        }
+    });
 });
